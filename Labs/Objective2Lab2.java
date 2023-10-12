8fb7d1754aaddaeeb4e1c294e1bc71e00488e6f3
Mandy <Lmandy239@gmail.com>
2023-10-12 18:08:01 -0400

    Objective 2 second lab

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Armando Laguardia" ;
+		int age = 22;
+		String state = "North Carolina" ;
+
+		System.out.println(name + " " + age + " " + state);
+		
+	}
+}
